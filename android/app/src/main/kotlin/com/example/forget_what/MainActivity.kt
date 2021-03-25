4d5ffab0d1aabfd57b4295fdf098ec8dd0896ba6
kaykat2000 <74114552+kaykat2000@users.noreply.github.com>
2021-03-25 18:52:01 -0400

    Revert "Trying a pull request"

--- android/app/src/main/kotlin/com/example/forget_what/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.forget_what
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/item_details.dart
+++ lib/item_details.dart
@@ -15,11 +15,7 @@ class _ItemDetails extends State<ItemDetails> {
   Widget build(BuildContext context) {
       return Scaffold(
         appBar: AppBar(title: Text('ItemDetails'),),
-<<<<<<< HEAD
-        body:  Text(item),      
-=======
         body:  Text("test"),
->>>>>>> a3fbb5a496afa8c7775152b7d83b8ff885e72f62
     );
   }
 }
